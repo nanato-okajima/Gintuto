5dabc0adebb8937c764a2e157f8ef296994b7a6b
naoto-naka <luckywhitecat770@gmail.com>
2022-02-25 19:17:25 +0900

    Add exception handling

--- api/controllers/products/product_controller_test.go
+++ api/controllers/products/product_controller_test.go
@@ -11,13 +11,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 
-	"Gintuto/domain/products"
+	"Gintuto/api/domain/products"
+	"Gintuto/api/utils/errors"
 )
 
-func TestCreateProduct(t *testing.T) {
-	p := products.Product{ID: 123, Name: "hoge taro"}
-	byteProduct, _ := json.Marshal(p)
+func requestHandler(p interface{}) (*gin.Context, *httptest.ResponseRecorder) {
 	res := httptest.NewRecorder()
+	byteProduct, _ := json.Marshal(p)
 	c, _ := gin.CreateTestContext(res)
 	c.Request, _ = http.NewRequest(
 		http.MethodPost,
@@ -25,13 +25,39 @@ func TestCreateProduct(t *testing.T) {
 		bytes.NewBuffer(byteProduct),
 	)
 
+	return c, res
+}
+
+func TestCreateProductNoError(t *testing.T) {
+	p := products.Product{ID: 123, Name: "hoge taro"}
+	c, res := requestHandler(p)
+
 	CreateProduct(c)
 
 	var product products.Product
 	err := json.Unmarshal(res.Body.Bytes(), &product)
-	assert.EqualValues(t, http.StatusOK, res.Code)
+	assert.EqualValues(t, http.StatusCreated, res.Code)
 	assert.Nil(t, err)
 	fmt.Println(product)
 	assert.EqualValues(t, uint64(123), product.ID)
+}
 
+func TestCreateProductWith404Error(t *testing.T) {
+	type demiProduct struct {
+		ID   string `json"id"`
+		Name string `json:"name"`
+	}
+
+	p := demiProduct{ID: "123", Name: "hoge taro"}
+	c, res := requestHandler(p)
+
+	CreateProduct(c)
+
+	var apiErr errors.ApiErr
+	err := json.Unmarshal(res.Body.Bytes(), &apiErr)
+	assert.EqualValues(t, http.StatusBadRequest, res.Code)
+	assert.Nil(t, err)
+	assert.EqualValues(t, "invalid json body", apiErr.Message)
+	assert.EqualValues(t, 400, apiErr.Status)
+	assert.EqualValues(t, "bad_request", apiErr.Error)
 }
