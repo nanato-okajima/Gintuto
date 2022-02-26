28e8689c09386507fc1b7e1e57664d38cb55c81e
naoto-naka <luckywhitecat770@gmail.com>
2022-02-26 11:40:08 +0900

    Add databse error handling

--- api/domain/products/product_dao.go
+++ api/domain/products/product_dao.go
@@ -3,7 +3,7 @@ package products
 import (
 	"Gintuto/api/datasources/mysql/products_db"
 	"Gintuto/api/utils/errors"
-	"fmt"
+	"Gintuto/api/utils/mysqlutils"
 )
 
 var (
@@ -12,7 +12,7 @@ var (
 
 func (p *Product) Get() *errors.ApiErr {
 	if result := products_db.Client.Where("id = ?", p.Model.ID).Find(&p); result.Error != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get product: %s", result.GetErrors()))
+		return mysqlutils.ParseError(result.Error)
 	}
 
 	return nil
@@ -20,9 +20,7 @@ func (p *Product) Get() *errors.ApiErr {
 
 func (p *Product) Save() *errors.ApiErr {
 	if result := products_db.Client.Create(&p); result.Error != nil {
-		return errors.NewInternalServerError(
-			fmt.Sprintf("error when trying to save product: %s", result.GetErrors()),
-		)
+		mysqlutils.ParseError(result.Error)
 	}
 
 	return nil
---
+++ api/utils/mysqlutils/mysql_utils.go
@@ -0,0 +1,28 @@
+package mysqlutils
+
+import (
+	"Gintuto/api/utils/errors"
+	"strings"
+)
+
+const (
+	noSearchResult = "record not found"
+)
+
+func ParseError(err error) *errors.ApiErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), noSearchResult) {
+			return errors.NewNotFoundError("no record matching given id")
+		}
+
+		return errors.NewInternalServerError("error parsing database response")
+	}
+
+	switch sqlErr.Number {
+	case 1062:
+		return errors.NewBadRequestError("invalid data")
+	}
+
+	return errors.NewInternalServerError("error processing request")
+}
